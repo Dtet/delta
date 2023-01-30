42e89f6dc0ee24e57ec559076a4f03de700d0d20
Everett <53265846+Dtet@users.noreply.github.com>
2023-01-29 23:19:13 -0400

    quinta subida

--- src/app/models/filter.model.ts
+++ src/app/models/filter.model.ts
@@ -1,20 +1,43 @@
 export interface IFiltro {
-    Facultativo: Number
-    Serie: Number
-    Exp: Number
-    Fe_Registro: Date
-    Domicilio: String
-    Moneda: Number
-    Ramo: String
-    Indole: String
-    Negocio: String
-    Cedente: String
-    AseguradoAfianzado: String
-    Suscriptor: String
-    Vigdesde:Date
-    Vighasta:Date
-    Estado: String
-    Nuevo: Boolean
-    Usuario: String
-    Poliza: String
+  Facultativo: Number;
+  Serie: Number;
+  Exp: Number;
+  Fe_Registro: Date;
+  Domicilio: String;
+  Moneda: Number;
+  Ramo: String;
+  Indole: String;
+  Negocio: String;
+  Cedente: String;
+  AseguradoAfianzado: String;
+  Suscriptor: String;
+  Vigdesde: Date;
+  Vighasta: Date;
+  Estado: String;
+  Nuevo: Boolean;
+  Usuario: String;
+  Poliza: String;
+}
+
+export interface IRadioQuestionOption {
+  key?: string;
+  label?: string;
+  order?: number;
+  options?: { key: string; value: string }[];
+}
+
+export interface IDropdownQuestionOptions {
+  key?: string;
+  label?: string;
+  options?: { key: string; value: string }[];
+  order?: number;
+}
+
+export interface ITextboxQuestionOptions {
+  key?: string;
+  label?: string;
+  order?: number;
+  type?: string;
+  required?: boolean;
+  value?: string;
 }
